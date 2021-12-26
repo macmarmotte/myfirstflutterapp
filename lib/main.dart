022fdaeeb71fa7d01208d2b44652b0db59c80646
macmarmotte <mathieucharpentier@yahoo.fr>
2021-12-26 23:05:24 +0100

    Initial commit
    Signed-off-by: macmarmotte <mathieucharpentier@yahoo.fr>

--- lib/main.dart
+++ lib/main.dart
@@ -11,7 +11,7 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Flutter Demo',
+      title: 'Flutter Demo 2',
       debugShowCheckedModeBanner: false,
       theme: ThemeData(
         primarySwatch: Colors.red,
